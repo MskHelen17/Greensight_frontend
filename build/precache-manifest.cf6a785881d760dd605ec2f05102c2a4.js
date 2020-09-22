c129209ed06cf44548e699e41e81bae0df10531e
Moskvitina Helen <Moskvitina.helen@gmail.com>
2020-09-22 12:02:47 +0300

    add build version of project

---
+++ build/precache-manifest.cf6a785881d760dd605ec2f05102c2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58c4947a90318ea682cb",
+    "url": "/static/css/main.85cab913.chunk.css"
+  },
+  {
+    "revision": "58c4947a90318ea682cb",
+    "url": "/static/js/main.9bb0f335.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13b6c69a0d55ab5f429c",
+    "url": "/static/js/2.c8587464.chunk.js"
+  },
+  {
+    "revision": "3b4524ad97051d07960027220271f6a9",
+    "url": "/index.html"
+  }
+];
